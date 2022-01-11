11c8a65397b83b9a4a0f10e38a7279aa289cc9e9
Van_yahite <ivansatinov@mail.ru>
2022-01-11 18:25:00 +0300

    Initial Commit

---
+++ MarvelHeroesCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHeroesCollection
+//
+//  Created by Иван Сатинов on 11.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
